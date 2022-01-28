7711d6186469b2c65f2608822a4a04fb859e713a
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2022-01-28 21:32:07 +0200

    Only allow movement to up,down,left,right

--- game objects/player.gd
+++ game objects/player.gd
@@ -8,11 +8,11 @@ func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		velocity.y += 1
-	if Input.is_action_pressed("up"):
+	elif Input.is_action_pressed("up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
