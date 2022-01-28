d327e383fc6d80b9b8da8615c342d846e24789d1
Anssi <anssi.hietaniemi@gmail.com>
2022-01-28 21:28:25 +0200

    Add ui/mainmenu.tscn

---
+++ ui/mainmenu.gd
@@ -0,0 +1,22 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    var button = Button.new()
+    button.text = "Painappa tästä"
+    button.connect("pressed", self, "_button_pressed")
+    add_child(button)
+
+func _button_pressed():
+	print("jeejeejeeejeeejee")
+	get_tree().change_scene("res://game objects/player.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
