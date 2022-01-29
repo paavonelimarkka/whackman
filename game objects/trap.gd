b368a891241284019771b6bb1ed0174e831bb76b
Arttu Rousku <M1484@student.jamk.fi>
2022-01-29 18:59:51 +0200

    add spider pooping traps

--- game objects/trap.gd
+++ game objects/trap.gd
@@ -7,9 +7,12 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print('asd')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _on_Area2D_body_entered(body):
+	print(body)
+	queue_free()
