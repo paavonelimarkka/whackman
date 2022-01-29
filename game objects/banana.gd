cfeef0b1d130aa42cc9c0af60df5325a2c6f21e2
Löysäranne <juho.talus@gmail.com>
2022-01-29 21:52:12 +0200

    Spawn banana when banana picked

--- game objects/banana.gd
+++ game objects/banana.gd
@@ -1,5 +1,7 @@
 extends Node
 
+onready var map = get_node("/root/Map")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("OLEN BANAANI")
@@ -9,4 +11,5 @@ func _on_body_entered(body):
 	if (body.get_name() == 'Player'):
 		print('banana_picked')
 		emit_signal('banana_picked')
+		map.spawn_banana()
 		queue_free()
