d384054846e77994d121d78bc903c4332dff66c0
Paavo <paavo.nelimarkka@gmail.com>
2022-01-29 20:16:11 +0200

    add apina

---
+++ apina.gd
@@ -0,0 +1,39 @@
+extends Node
+
+export var nearTime = 2
+export var farTime = 4
+
+var nearCamTimer = Timer.new()
+var farCamTimer = Timer.new()
+
+signal near_cam_on
+signal far_cam_on
+
+
+func _ready():
+	add_child(nearCamTimer)
+	add_child(farCamTimer)
+	nearCamTimer.set_wait_time(nearTime)
+	farCamTimer.set_wait_time(farTime)
+	
+	nearCamTimer.connect("timeout", self, "_on_near_timer_timeout")
+	farCamTimer.connect("timeout", self, "_on_far_timer_timeout")
+	
+	nearCamTimer.start()
+	
+
+func _on_near_timer_timeout():
+	$AnimatedSprite.frame = 0
+	$AnimatedSprite.play("whack")
+	nearCamTimer.stop()
+	farCamTimer.start()
+	emit_signal("far_cam_on")
+	print("pam")
+	
+func _on_far_timer_timeout():
+	$AnimatedSprite.frame = 0
+	$AnimatedSprite.play("whack")
+	farCamTimer.stop()
+	nearCamTimer.start()
+	emit_signal("near_cam_on")
+	print("pum")
