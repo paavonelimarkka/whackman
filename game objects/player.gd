d2a2ca8dc00fa5a28dd2c18dccb26ba027bc0a67
Arttu Rousku <M1484@student.jamk.fi>
2022-01-29 13:43:49 +0200

    add trap slow down

--- game objects/player.gd
+++ game objects/player.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 export (int) var speed = 200
 
 var velocity = Vector2()
+var timer = Timer.new()
+
+func _ready():
+	timer.connect("timeout",self,"_on_timer_timeout") 
+	add_child(timer) #to process
 
 func get_input():
 	velocity = Vector2()
@@ -20,6 +25,16 @@ func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 
-	for index in get_slide_count():
-		var collision = get_slide_collision(index)
-		print(collision)
+
+	#for index in get_slide_count():
+	#	var collision = get_slide_collision(index)
+	#	if collision.collider.is_in_group("traps"):
+	#		slow_down()
+	#		print(collision.collider.name)
+			
+func slow_down():
+	timer.start() #to start
+	speed = 100
+
+func _on_timer_timeout():
+	speed = 200
