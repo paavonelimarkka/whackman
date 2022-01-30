7819860d9c36619dd8a40f77cecfda0a6c9fb657
Löysäranne <juho.talus@gmail.com>
2022-01-30 13:29:29 +0200

    Add better z-indexing for game objects

--- game objects/banana.gd
+++ game objects/banana.gd
@@ -5,13 +5,10 @@ onready var hud = get_node("/root/Map/HUD")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("OLEN BANAANI")
 	connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body):
 	if (body.get_name() == 'Player'):
-		print('banana_picked')
-		emit_signal('banana_picked')
 		map.spawn_banana()
 		hud.update_banana_counter()
 		queue_free()
